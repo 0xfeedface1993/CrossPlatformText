96951fc22a2cf92232d1a6ca56dec3216b78ab5f
许庭淞 <318715498@qq.com>
2018-05-08 11:31:31 +0800

    Initial Commit

---
+++ CrossPlatformText/CrossPlatformText.h
@@ -0,0 +1,19 @@
+//
+//  CrossPlatformText.h
+//  CrossPlatformText
+//
+//  Created by virus1994 on 2018/5/8.
+//  Copyright © 2018年 ascp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CrossPlatformText.
+FOUNDATION_EXPORT double CrossPlatformTextVersionNumber;
+
+//! Project version string for CrossPlatformText.
+FOUNDATION_EXPORT const unsigned char CrossPlatformTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrossPlatformText/PublicHeader.h>
+
+
